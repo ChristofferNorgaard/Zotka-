df9d677375f2f60cc56f2cd973cc4a67d26f7747
Smrt666 <64538187+Smrt666@users.noreply.github.com>
2020-06-02 08:21:12 +0200

    Pripravljena na sodelovanje z novim žpp-jem
    Podpira (upam) kar potrebuje žpp.

--- grammar.py
+++ grammar.py
@@ -4,7 +4,7 @@ from standardLibrary.narVar import NAR
 from standardLibrary.uloVar import ULO
 from standardLibrary.functions import *
 
-type_to_var = {'int':INT, 'nar':NAR, 'dec':'', 'ulo':ULO, 'flo':'', 'kom':'', 'bul':'', 'črk':'', 'bes':'', 'niz':str, 'sez':list, 'mno':set, 'slo':dict}
+type_to_var = {'int':INT, 'nar':NAR, 'ulo':ULO, 'flo':float, 'niz':str, 'sez':list, 'mno':set, 'slo':dict}
 name_to_func = {'izpiši|T' : printf, 'preberi|T' : inputf, 'preberi_celo_število|T' : inputint, 'preberi_ulomek|T':inputulo, 'izpiši|izplakni|T': print_and_flush, 'izpiši|z določenim koncem in izplakni(da/ne)T' : superprint, 'izpiši_celo_število|T' : printint}
 minus_to_f = { '-' : krat, '--' : plus, '---': minus, '----': deljeno, '-----': na, '------': koren, '-------': faktorel, '--------': št_diagonal, '---------': ploščina_elipse, '----------': deljeno_z_ena}
 
@@ -36,23 +36,27 @@ def generate_variables(s):
 	name = ''
 	value = ''
 	x=0
-	while x < len(s):
+	while not tip in Variable.types: #iskanje tipa
 		tip+=s[x]
-		if tip in Variable.types:
-			tip=(type_to_var[tip])
-			name=''
-			x+=1
-			while s[x] != '[':
-				name += s[x]
-				x+=1
-			x+=1
-			value=''
-			while s[x] != '}':
-				value+=s[x]
-				x+=1
-			fn.append((tip(value), name))
-			tip = ''
 		x+=1
+	tip=(type_to_var[tip])
+
+	while s[x] != '[': # iskanje imena
+		name += s[x]
+		x+=1
+	x+=1
+	
+	while s[x] != '}': # iskanje vrednosti
+		value+=s[x]
+		x+=1
+	########################### Preverjanje, da ni številka napisana s številkami
+	if tip in [INT, NAR, ULO]:
+		for x in range(10):
+			if x in value:
+				raise Exception('Narobe napisana številka.')
+	###########################
+	fn.append((tip(value), name))
+	tip = ''
 	return fn
 	
 
@@ -76,6 +80,13 @@ class Line:
 			self.tip = 'calc'
 		elif levo in ['komentiraj', 'prosim komentiraj']:
 			self.tip = 'komentar'
+		elif levo in ['poskoči na', 'prosim poskoči na']:
+			self.tip = 'jmp'
+		elif levo in ['poskoči za', 'prosim poskoči za']:
+			self.tip = 'hop' 
+		elif levo == 'Prosim poslovi se od nas in umri, potem se vrni kot velika ŽOTKA, da te bomo lahko še uporabljali in imeli radi.':
+			print('<ž++: Zbogom. Živite brez mene do mojega ponovnega klica.>')
+			self.tip = 'stop'
 
 	def __str__(self):
 		return str(self.line)
